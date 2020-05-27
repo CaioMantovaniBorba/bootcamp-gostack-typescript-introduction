204c8f5af9a01f31ad4f175d2355ec4016aed50c
Caio Mantovani <caiovmborba@gmail.com>
2020-05-27 13:16:50 -0300

    Type objects and vectors

--- dist/routes.js
+++ dist/routes.js
@@ -1,7 +1,21 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.helloWorld = void 0;
+var CreateUser_1 = __importDefault(require("./services/CreateUser"));
 function helloWorld(req, res) {
+    var user = CreateUser_1.default({
+        email: 'caiovmborba@gmail.com',
+        password: '123',
+        techs: [
+            'NodeJS',
+            'ReactJS',
+            'React Native',
+            { title: 'JavaScript', experience: 100 },
+        ],
+    });
     return res.json({ message: 'Hello World!' });
 }
 exports.helloWorld = helloWorld;
--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,17 @@
 import { Request, Response } from 'express';
+import createUser from './services/CreateUser';
 
 export function helloWorld(req: Request, res: Response) {
+  const user = createUser({
+    email: 'caiovmborba@gmail.com',
+    password: '123',
+    techs: [
+      'NodeJS',
+      'ReactJS',
+      'React Native',
+      { title: 'JavaScript', experience: 100 },
+    ],
+  });
+
   return res.json({ message: 'Hello World!' });
 }
